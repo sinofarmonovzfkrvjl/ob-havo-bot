00bfbf40e3f3d5479b723b8d8395a1a0f8bf6f85
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-08-01 07:16:37 +0500

    commit

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 import logging
 import asyncio
-from weather import Weather
+from weather import UzbekistanWeather
 from keyboards import cities_button
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-weather = Weather()
+weather = UzbekistanWeather()
 bot = Bot(os.getenv("TOKEN"))
 dp = Dispatcher()
 
@@ -24,36 +24,36 @@ async def start(message: types.Message):
 @dp.callback_query()
 async def CallBack(call: types.CallbackQuery):
     if call.data == "toshkent":
-        await call.message.answer(f"bugun toshkentda ob havo harorati {weather.toshkent()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun toshkentda ob havo harorati {UzbekistanWeather('toshkent').toshkent()} bo'lishi kutilmoqda")
     elif call.data == "andijon":
-        await call.message.answer(f"bugun Andijon ob havo harorati {Weather('andijon').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Andijon ob havo harorati {UzbekistanWeather('andijon').today()} bo'lishi kutilmoqda")
     elif call.data == "buxoro":
-        await call.message.answer(f"bugun Buxoro ob havo harorati {Weather('buxoro').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Buxoro ob havo harorati {UzbekistanWeather('buxoro').today()} bo'lishi kutilmoqda")
     elif call.data == "guliston":
-        await call.message.answer(f"bugun Guliston ob havo harorati {Weather('guliston').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Guliston ob havo harorati {UzbekistanWeather('guliston').today()} bo'lishi kutilmoqda")
     elif call.data == "jizzax":
-        await call.message.answer(f"bugun Jizzax ob havo harorati {Weather('jizzax').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Jizzax ob havo harorati {UzbekistanWeather('jizzax').today()} bo'lishi kutilmoqda")
     elif call.data == "zarafshon":
-        await call.message.answer(f"bugun Zarafshon ob havo harorati {Weather('zarafshon').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Zarafshon ob havo harorati {UzbekistanWeather('zarafshon').today()} bo'lishi kutilmoqda")
     elif call.data == "qarshi":
-        await call.message.answer(f"bugun Qarshi ob havo harorati {Weather('qarshi').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Qarshi ob havo harorati {UzbekistanWeather('qarshi').today()} bo'lishi kutilmoqda")
     elif call.data == "navoiy":
-        await call.message.answer(f"bugun Navoiy ob havo harorati {Weather('navoiy').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Navoiy ob havo harorati {UzbekistanWeather('navoiy').today()} bo'lishi kutilmoqda")
     elif call.data == "namangan":
-        await call.message.answer(f"bugun Namangan ob havo harorati {Weather('namangan').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Namangan ob havo harorati {UzbekistanWeather('namangan').today()} bo'lishi kutilmoqda")
     elif call.data == "nukus":
-        await call.message.answer(f"bugun Nukus ob havo harorati {Weather('nukus').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Nukus ob havo harorati {UzbekistanWeather('nukus').today()} bo'lishi kutilmoqda")
     elif call.data == "samarqand":
-        await call.message.answer(f"bugun Samarqand ob havo harorati {Weather('samarqand').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Samarqand ob havo harorati {UzbekistanWeather('samarqand').today()} bo'lishi kutilmoqda")
     elif call.data == "termiz":
-        await call.message.answer(f"bugun Termiz ob havo harorati {Weather('termiz').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Termiz ob havo harorati {UzbekistanWeather('termiz').today()} bo'lishi kutilmoqda")
     elif call.data == "urganch":
-        await call.message.answer(f"bugun Urganch ob havo harorati {Weather('urganch').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Urganch ob havo harorati {UzbekistanWeather('urganch').today()} bo'lishi kutilmoqda")
     elif call.data == "farg'ona":
-        fargona = Weather('farg\'ona').today()
+        fargona = UzbekistanWeather('farg\'ona').today()
         await call.message.answer(f"bugun Farg'ona ob havo harorati {fargona} bo'lishi kutilmoqda")
     elif call.data == "xiva":
-        await call.message.answer(f"bugun Xiva ob havo harorati {Weather('xiva').today()} bo'lishi kutilmoqda")
+        await call.message.answer(f"bugun Xiva ob havo harorati {UzbekistanWeather('xiva').today()} bo'lishi kutilmoqda")
     await call.answer("")
 
 @dp.shutdown()
